3c55067ef235114e76c6bbf332840286c2f64e70
EmmanueleBollino <emmanuele.bollino@studenti.unicam.it>
2020-08-21 16:21:02 +0200

    Port

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import express from 'express';
 import solc from 'solc';
 
 const app = express();
-const port = 80;
+//const port = 80;
+
+const server = require('http').createServer();
+const port = process.env.PORT || 80;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
